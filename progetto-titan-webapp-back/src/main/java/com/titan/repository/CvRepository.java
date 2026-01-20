15b06b4d2fa0d169df8662d58c867fb913d1f2f4
Andrea Altomare <164928731+andrea-340@users.noreply.github.com>
2026-01-20 14:07:40 +0100

    Delete progetto-titan-webapp-back directory

--- progetto-titan-webapp-back/src/main/java/com/titan/repository/CvRepository.java
+++
@@ -1,9 +0,0 @@
-package com.titan.repository;
-
-import com.titan.model.Cv;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CvRepository extends JpaRepository<Cv, Long> {
-}
