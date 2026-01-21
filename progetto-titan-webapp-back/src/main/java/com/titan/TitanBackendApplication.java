5d4eed77487205220fd2ab57c7642ad49ff3d35c
Andrea Altomare <164928731+andrea-340@users.noreply.github.com>
2026-01-21 01:05:49 +0100

    Add files via upload

---
+++ progetto-titan-webapp-back/src/main/java/com/titan/TitanBackendApplication.java
@@ -0,0 +1,12 @@
+package com.titan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TitanBackendApplication.class, args);
+    }
+}
