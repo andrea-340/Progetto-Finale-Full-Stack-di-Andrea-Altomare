f3df8f71b8cdb9121fd544bbd51e5248ca429463
Andrea Altomare <164928731+andrea-340@users.noreply.github.com>
2026-01-21 02:26:04 +0100

    Fix import statement for index.css

--- progetto-titan-webapp-front/src/index.js
+++ progetto-titan-webapp-front/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css"; // ⬅️ QUESTA RIGA DEVE ESSERCI
+import "./index.css"; 
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
