839f22c2d1f7c61d416b338c12d9e175d68202d2
Andrea Altomare <164928731+andrea-340@users.noreply.github.com>
2026-01-21 01:05:28 +0100

    Add files via upload

---
+++ src/main/java/com/titan/repository/CvRepository.java
@@ -0,0 +1,9 @@
+package com.titan.repository;
+
+import com.titan.model.Cv;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CvRepository extends JpaRepository<Cv, Long> {
+}
